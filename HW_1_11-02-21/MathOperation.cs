90213484d966f8f212b24f811e5f16f27cc53c41
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-02-12 13:14:29 +0500

    Добавьте файлы проекта.

---
+++ HW_1_11-02-21/MathOperation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_1_11_02_21
+{
+    class MathOperation<T>
+    {
+            public delegate T Operation(T firstNumber, T secondNumber);
+            /// <summary>
+            /// Added two numbers
+            /// </summary>
+            /// <param name="firstNumber"></param>
+            /// <param name="secondNumber"></param>
+            /// <returns></returns>
+            private static T Add(T firstNumber, T secondNumber)         
+            {
+                Console.Write($"{firstNumber} + {secondNumber} = ");
+                return (dynamic)firstNumber + (dynamic)secondNumber;
+            }
+            /// <summary>
+            /// Subtract two numbers
+            /// </summary>
+            /// <param name="firstNumber"></param>
+            /// <param name="secondNumber"></param>
+            /// <returns></returns>
+            private static T Subtract(T firstNumber, T secondNumber)
+            {
+                Console.Write($"{firstNumber} - {secondNumber} = ");
+                return (dynamic)firstNumber - (dynamic)secondNumber;
+            }
+            /// <summary>
+            /// Multiply two numbers
+            /// </summary>
+            /// <param name="firstNumber"></param>
+            /// <param name="secondNumber"></param>
+            /// <returns></returns>
+            private static T Multiply(T firstNumber, T secondNumber)
+            {
+                Console.Write($"{firstNumber} * {secondNumber} = ");
+                return (dynamic)firstNumber * (dynamic)secondNumber;
+            }
+            /// <summary>
+            /// Divide two numbers
+            /// </summary>
+            /// <param name="firstNumber"></param>
+            /// <param name="secondNumber"></param>
+            /// <returns></returns>
+            private static T Divide(T firstNumber, T secondNumber)
+            {
+                if ((dynamic)secondNumber == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"Деление на нол !");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return (dynamic)secondNumber;
+                }
+                else
+                {
+                    Console.Write($"{firstNumber} / {secondNumber} = ");
+                    return (dynamic)firstNumber / (dynamic)secondNumber;
+                }
+            }
+            public Operation operSum = Add;
+            public Operation operMin = Subtract;
+            public Operation operMul = Multiply;
+            public Operation operDiv = Divide;
+        }
+    }
+
---
+++ HW_1_11-02-21/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HW_1_11_02_21
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation<double> Operation = new MathOperation<double>();
+            while (true)
+            {
+                Console.Write("Первое число: "); double firstNumber = double.Parse(Console.ReadLine());
+                Console.Write("Второе число: "); double secondNumber = double.Parse(Console.ReadLine());
+                Console.WriteLine(@"Выберите комманду:
+                    1. Суммирование ---> +
+                    2. Вычитание    ---> -
+                    3. Умножение    ---> *
+                    4. Деление      ---> /
+                    5. Выход        ---> !");
+                switch (Console.ReadLine())
+                {
+                    case "+": Console.WriteLine(Operation.operSum(firstNumber, secondNumber)); break;
+                    case "-": Console.WriteLine(Operation.operMin(firstNumber, secondNumber)); break;
+                    case "*": Console.WriteLine(Operation.operMul(firstNumber, secondNumber)); break;
+                    case "/": Console.WriteLine(Operation.operDiv(firstNumber, secondNumber)); break;
+                    case "!": return;
+                }
+            }
+        }
+    }
+}
